210a87cc25d38849ab4a5ca3d9a075468baf00e3
zzz <zzz@192.168.2.107>
2019-12-11 22:44:26 +0100

    backup 4

---
+++ soap-web-services/backup04-define-jaxb-plugin-and-a-endpoint/backup04-define-jaxb-plugin-and-a-endpoint/src/main/java/com/in28minutes/courses/ObjectFactory.java
@@ -0,0 +1,63 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2019.12.11 a las 10:40:35 PM GMT+01:00 
+//
+
+
+package com.in28minutes.courses;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.in28minutes.courses package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.in28minutes.courses
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link GetCourseDetailsRequest }
+     * 
+     */
+    public GetCourseDetailsRequest createGetCourseDetailsRequest() {
+        return new GetCourseDetailsRequest();
+    }
+
+    /**
+     * Create an instance of {@link GetCourseDetailsResponse }
+     * 
+     */
+    public GetCourseDetailsResponse createGetCourseDetailsResponse() {
+        return new GetCourseDetailsResponse();
+    }
+
+    /**
+     * Create an instance of {@link CourseDetails }
+     * 
+     */
+    public CourseDetails createCourseDetails() {
+        return new CourseDetails();
+    }
+
+}
---
+++ soap-web-services/backup04-define-jaxb-plugin-and-a-endpoint/backup04-define-jaxb-plugin-and-a-endpoint/src/main/java/com/in28minutes/courses/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2019.12.11 a las 10:40:35 PM GMT+01:00 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://in28minutes.com/courses", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.in28minutes.courses;
